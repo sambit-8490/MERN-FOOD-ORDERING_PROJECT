aa3ddc1996d677e246c060426c40566372b06c7c
Prashon <prashon109939@gmail.com>
2024-11-15 09:40:52 +0545

    Stripe and Order Payment

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,19 +8,21 @@ import PlaceOrder from "./pages/PlaceOrder/PlaceOrder";
 import { Route } from "react-router-dom";
 import Footer from "./components/Footer/Footer";
 import LoginPopup from "./components/LoginPopup/LoginPopup";
+import Verify from "./pages/Verify/Verify";
 
 const App = () => {
   const [showLogin, setShowLogin] = useState(false);
 
   return (
     <>
-      {showLogin ? <LoginPopup setShowLogin={setShowLogin}/> : <></>}
+      {showLogin ? <LoginPopup setShowLogin={setShowLogin} /> : <></>}
       <div className="app">
-        <Navbar setShowLogin={setShowLogin}/>
+        <Navbar setShowLogin={setShowLogin} />
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/cart" element={<Cart />} />
           <Route path="/order" element={<PlaceOrder />} />
+          <Route path="/verify" element={<Verify />} />
         </Routes>
       </div>
       <Footer />
