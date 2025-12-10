405193cb53361f5ee5ed201f911e1d5df3a09ff5
Developer <massssss@gmail.com>
2025-12-10 11:48:38 +0530

    update file

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,5 +1,14 @@
 import mongoose from "mongoose";
 
-export const connectDB = async () =>{
-  await mongoose.connect('mongodb+srv://prashon10:109939@cluster0.fjeru.mongodb.net/food-ordering-project').then(()=>console.log("DB Connected!"));
-}
+export const connectDB = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("MongoDB connected!");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+    process.exit(1); // Exit if DB connection fails
+  }
+};
